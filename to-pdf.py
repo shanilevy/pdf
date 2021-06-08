b83e4168eed184984d25ad3215f16877eb8a0612
Felipe Ryan <feliperyan@google.com>
2021-06-08 14:18:24 +1000

    loader.io handler

--- to-pdf.py
+++ to-pdf.py
@@ -12,6 +12,7 @@ ALLOWED_EXTENSIONS = set(['doc', 'docx', 'xls', 'xlsx'])
 
 app = Flask(__name__)
 
+LOADER_IO_TOKEN = os.environ.get('LOADER_IO_TOKEN', "who knows")
 
 # Convert using Libre Office
 def convert_file(output_dir, input_file):
@@ -60,6 +61,13 @@ def api():
         return response
  
     return send_file(output_file_path, mimetype='application/pdf')
+
+
+# Added this for loader.io verification
+# it expects a url endpoint similar to 'loader.io-f408b6...'
+@app.route('LOADER_IO_TOKEN', methods=['GET'])
+def loader_io_verification():
+    return LOADER_IO_TOKEN
 
 
 if __name__ == "__main__":
